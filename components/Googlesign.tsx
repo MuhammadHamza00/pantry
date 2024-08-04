a47ef854aa5000368c83df31ad3e8284951a7fc3
Muhammad Hamza <91649627+MuhammadHamza00@users.noreply.github.com>
2024-08-04 16:44:27 +0500

    Update Googlesign.tsx

--- components/Googlesign.tsx
+++ components/Googlesign.tsx
@@ -3,7 +3,7 @@
 import { useState } from 'react';
 import { Button, CircularProgress, Snackbar, Alert } from '@mui/material';
 import { signInWithPopup } from 'firebase/auth';
-import { auth, provider } from '../../firebase'; // Adjust the import path as necessary
+import { auth, provider } from '../firebase'; // Adjust the import path as necessary
 
 const GoogleSignIn = () => {
   const [loading, setLoading] = useState(false);
